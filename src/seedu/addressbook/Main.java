898c3c8ccc432c5378b3fc74ef43e31c0f90b2a7
Yu Hong Herald <yuhongherald@gmail.com>
2018-02-07 11:44:34 +0800

    Add details to error handling for read-only on save

--- src/seedu/addressbook/Main.java
+++ src/seedu/addressbook/Main.java
@@ -58,6 +58,7 @@ public class Main {
             ui.showWelcomeMessage(VERSION, storage.getPath());
 
         } catch (InvalidStorageFilePathException | StorageOperationException e) {
+            System.out.println(e.getMessage());
             ui.showInitFailedMessage();
             /*
              * ==============NOTE TO STUDENTS=========================================================================
@@ -105,15 +106,23 @@ public class Main {
      * @param command user command
      * @return result of the command
      */
-    private CommandResult executeCommand(Command command)  {
-        try {
+    private CommandResult executeCommand(Command command)
+    {
+        try
+        {
             command.setData(addressBook, lastShownList);
             CommandResult result = command.execute();
             storage.save(addressBook);
             return result;
-        } catch (Exception e) {
+        }
+        catch (StorageOperationException e) {
             ui.showToUser(e.getMessage());
-            throw new RuntimeException(e);
+            System.out.println("File is read-only. Change file permissions and retry.");
+            throw new RuntimeException();
+        }
+        catch (UnsupportedOperationException e) {
+            ui.showToUser(e.getMessage());
+            throw new RuntimeException();
         }
     }
 
